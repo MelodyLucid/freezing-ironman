aaadd4bbabe6e6f4676f496f22a9903a83145784
Vlad Ureche <vlad.ureche@gmail.com>
2014-03-30 22:58:50 +0200

    Test name shown and more pre-bench warmup

--- project/Build.scala
+++ project/Build.scala
@@ -23,9 +23,9 @@ object MyBuild extends Build {
     libraryDependencies += "org.scala-miniboxing.plugins" %% "miniboxing-runtime" % "0.2-SNAPSHOT",
     addCompilerPlugin("org.scala-miniboxing.plugins" %% "miniboxing-plugin" % "0.2-SNAPSHOT"),
     scalacOptions ++= (
-      "-P:minibox:log" ::    // enable the miniboxing plugin output
+      //"-P:minibox:log" ::    // enable the miniboxing plugin output
                              // (which explains what the plugin is doing
-      "-Xprint:minibox-spec" ::
+      //"-Xprint:minibox-spec" ::
       //"-P:minibox:hijack" :: // enable hijacking the @specialized annotations
       //                       // transforming them into @miniboxed annotations
       //"-optimize" ::         // necessary to get the best performance when
--- src/test/scala/miniboxing/test/RangeBenchmark.scala
+++ src/test/scala/miniboxing/test/RangeBenchmark.scala
@@ -3,18 +3,20 @@ package miniboxing.test
 import org.scalameter.api._
 import miniboxing.text.infrastructure.FreezingTest
 
-object RangeBenchmark extends FreezingTest {
-  val sizes: Gen[Int] = Gen.range("size")(300000, 1500000, 300000)
-
-  val ranges: Gen[Range] = for {
-    size <- sizes
-  } yield 0 until size
-
-  performance of "Range" in {
-    measure method "map" in {
-      using(ranges) in {
-        r => r.map(_ + 1)
-      }
-    }
-  }
-}
+// we don't need this guy wasting our time with each full test:
+//
+//object RangeBenchmark extends FreezingTest {
+//  val sizes: Gen[Int] = Gen.range("size")(300000, 1500000, 300000)
+//
+//  val ranges: Gen[Range] = for {
+//    size <- sizes
+//  } yield 0 until size
+//
+//  performance of "Range" in {
+//    measure method "map" in {
+//      using(ranges) in {
+//        r => r.map(_ + 1)
+//      }
+//    }
+//  }
+//}
