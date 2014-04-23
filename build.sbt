f87c2e936f18f6ce98c15565e4b5c394d5e51a3d
Vlad Ureche <vlad.ureche@gmail.com>
2014-04-23 14:46:58 +0200

    Updated project structure

--- build.sbt
+++ build.sbt
@@ -1,11 +1,4 @@
-libraryDependencies += "com.github.axel22" %% "scalameter" % "0.4"
+organization := "ch.epfl.lamp"
 
-libraryDependencies += "org.scalatest" % "scalatest_2.10" % "2.1.0" % "test"
+version := "0.0.1-SNAPSHOT"
 
-libraryDependencies ++= Seq(
-   "junit" % "junit" % "4.8.1" % "test"
-)
-
-testFrameworks += new TestFramework("org.scalameter.ScalaMeterFramework")
-
-parallelExecution in Test := false
--- project/Build.scala
+++ project/Build.scala
@@ -1,36 +1,83 @@
 import sbt._
-import sbt.Keys._
+import Keys._
+import Process._
 
-object MyBuild extends Build {
+object FreezingIronmanBuild extends Build {
 
-  /** This is the main project */
-  lazy val root: Project = Project(
-    "freezing-ironman",
-    file("."),
-    settings = Defaults.defaultSettings ++ Seq[Setting[_]](
-      organization := "ch.epfl.lamp",
-      version := "0.0.1-SNAPSHOT",
-      // The plugin requires the latest version of the scalac compiler. You
-      // can use older compilers, but before reporting a bug, please check
-      // that it can be reproduced with the latest version of the compiler.
-      scalaVersion := "2.10.3"
-    ) ++ miniboxingSettings
-  )
+  val scalaVer = "2.10.4"
+  val paradiseVersion = "2.0.0"
 
-  /** Settings for the miniboxing plugin */
-  lazy val miniboxingSettings = Seq[Setting[_]](
+  val defaults = Defaults.defaultSettings ++ Seq(
+    scalaVersion := scalaVer,
+    scalaSource in Compile := baseDirectory.value / "src",
+    javaSource in Compile := baseDirectory.value / "src",
+    scalaSource in Test := baseDirectory.value / "test",
+    javaSource in Test := baseDirectory.value / "test",
+    resourceDirectory in Compile := baseDirectory.value / "resources",
+    compileOrder := CompileOrder.JavaThenScala,
+
+    unmanagedSourceDirectories in Compile := Seq((scalaSource in Compile).value),
+    unmanagedSourceDirectories in Test := Seq((scalaSource in Test).value),
+    //http://stackoverflow.com/questions/10472840/how-to-attach-sources-to-sbt-managed-dependencies-in-scala-ide#answer-11683728
+    com.typesafe.sbteclipse.plugin.EclipsePlugin.EclipseKeys.withSource := true,
+
+    resolvers in ThisBuild ++= Seq(
+      Resolver.sonatypeRepo("releases"),
+      Resolver.sonatypeRepo("snapshots")
+    ),
+
+    libraryDependencies ++= Seq(
+      // "org.scalacheck" %% "scalacheck" % "1.10.0" % "test",
+      "com.novocode" % "junit-interface" % "0.10-M2" % "test"
+    ),
+
+    scalacOptions ++= Seq("-feature", "-deprecation", "-unchecked", "-Xlint"),
+
+    publishArtifact in packageDoc := false,
+
+    parallelExecution in Test := false,
+    testOptions += Tests.Argument(TestFrameworks.JUnit, "-q", "-v"),
+
+    libraryDependencies ++= Seq(
+      "org.scala-lang" % "scala-library" % scalaVer,
+      "org.scala-lang" % "scala-reflect" % scalaVer,
+      "org.scala-lang" % "scala-compiler" % scalaVer,
+      // "org.scala-lang" % "scala-partest" % scalaVer,
+      "com.googlecode.java-diff-utils" % "diffutils" % "1.2.1"
+    ),
+
+    // macros paradise:
+    addCompilerPlugin("org.scalamacros" % "paradise" % paradiseVersion cross CrossVersion.full),
+    libraryDependencies ++= (
+      if (scalaVersion.value.startsWith("2.10")) List("org.scalamacros" %% "quasiquotes" % paradiseVersion)
+      else Nil
+    ),
+
+    // miniboxing plugin:
     resolvers += Resolver.sonatypeRepo("snapshots"),
     libraryDependencies += "org.scala-miniboxing.plugins" %% "miniboxing-runtime" % "0.2-SNAPSHOT",
     addCompilerPlugin("org.scala-miniboxing.plugins" %% "miniboxing-plugin" % "0.2-SNAPSHOT"),
     scalacOptions ++= (
       "-P:minibox:log" ::    // enable the miniboxing plugin output
                              // (which explains what the plugin is doing
-      "-Xprint:minibox-spec" ::
+      //"-Xprint:minibox-spec" ::
       //"-P:minibox:hijack" :: // enable hijacking the @specialized annotations
       //                       // transforming them into @miniboxed annotations
       //"-optimize" ::         // necessary to get the best performance when
                              // using the miniboxing plugin
       Nil
-    )
+    ),
+
+    // scalameter and scalatest:
+    libraryDependencies ++= Seq("com.github.axel22" %% "scalameter" % "0.4",
+                                "org.scalatest" %% "scalatest" % "2.1.0" % "test",
+                                "junit" % "junit" % "4.8.1" % "test"),
+    testFrameworks += new TestFramework("org.scalameter.ScalaMeterFramework"),
+    parallelExecution in Test := false
   )
+
+  lazy val _freezing   = Project(id = "freezing",            base = file(".")) aggregate (macros, benchmarks, scratchpad)
+  lazy val macros      = Project(id = "freezing-macros",     base = file("components/macros"), settings = defaults)
+  lazy val benchmarks  = Project(id = "freezing-benchmarks", base = file("components/benchmarks"), settings = defaults) dependsOn (macros)
+  lazy val scratchpad  = Project(id = "freezing-scratchpad", base = file("components/scratchpad"), settings = defaults)
 }
