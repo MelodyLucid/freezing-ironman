78844ee7693b339449512b876d54f59a4ec649ed
MelodyLucid <melo.lucid@gmail.com>
2014-06-04 11:17:01 +0200

    Less verbose.

--- project/Build.scala
+++ project/Build.scala
@@ -58,9 +58,10 @@ object FreezingIronmanBuild extends Build {
     libraryDependencies += "org.scala-miniboxing.plugins" %% "miniboxing-runtime" % "0.2-SNAPSHOT",
     addCompilerPlugin("org.scala-miniboxing.plugins" %% "miniboxing-plugin" % "0.2-SNAPSHOT"),
     scalacOptions ++= (
-      "-P:minibox:log" ::    // enable the miniboxing plugin output
+      //"-P:minibox:log" ::    // enable the miniboxing plugin output
                              // (which explains what the plugin is doing
-      "-Xprint:minibox-spec" ::
+      //"-P:minibox:two-way" :: // enable even better performance
+      //"-Xprint:minibox-spec" ::
       "-P:minibox:hijack" :: // enable hijacking the @specialized annotations
       //                       // transforming them into @miniboxed annotations
       "-optimize" ::         // necessary to get the best performance when
