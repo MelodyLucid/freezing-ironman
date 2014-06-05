ac07b41662c0d1558ca68403be3458b81f7f8a3e
Vlad Ureche <vlad.ureche@gmail.com>
2014-06-05 19:36:02 +0200

    HashMap's not a moving target anymore

--- components/scratchpad/test/miniboxing/test/paper/HackMapBenchmark.scala
+++ components/scratchpad/test/miniboxing/test/paper/HackMapBenchmark.scala
@@ -1,159 +1,88 @@
 package miniboxing.test.paper
 
 import miniboxing.test.infrastructure.FreezingTest
+import miniboxing.hackmap._
 import org.scalameter.Gen
-import miniboxing.hackmap.HackMap
 
 class HackMapBenchmark extends FreezingTest {
 
   def rand(seed: Int) = new scala.util.Random(seed)
 
+  var hackMap: HackMapGeneric[Int, Double] = null
+  var hashMap: stolen.HashMap[Int, Double] = null
 
-  // Empty HashMaps
+  val size =  Gen.single("size")(500000)
 
-  val genericHackMap = new HackMap.Generic[Int, Double]
-  val specializedHackMap = new HackMap.Specialized[Int, Double]
-  val miniboxedHackMap = new HackMap.Miniboxed[Int, Double]
-  val javaHashMap = new java.util.HashMap[Int, Double]
-
-  val genericGen = Gen.single("Empty Generic HackMap")(genericHackMap)
-  val specializedGen = Gen.single("Empty Specilaized HackMap")(specializedHackMap)
-  val miniboxedGen = Gen.single("Empty Miniboxed HackMap")(miniboxedHackMap)
-  val javaGen = Gen.single("Empty Java HashMap")(javaHashMap)
-
-
-  // Randomly predefined HashMaps
-
-  val rdmGenericHackMap = new HackMap.Generic[Int, Double]
-  val rdmSpecializedHackMap = new HackMap.Specialized[Int, Double]
-  val rdmMiniboxedHackMap = new HackMap.Miniboxed[Int, Double]
-  val rdmJavaHashMap = new java.util.HashMap[Int, Double]
-
-  // The following fills the HashMaps with the same random values
-  {
-    val random = rand(1)
-
-    for (i <- 0 until 1000) {
-      val k = random.nextInt
-      val v = random.nextDouble
-
-      rdmGenericHackMap.put(k, v)
-      rdmSpecializedHackMap.put(k, v)
-      rdmMiniboxedHackMap.put(k, v)
-      rdmJavaHashMap.put(k, v)
-    }
-  }
-
-
-  val rdmGenericGen = Gen.single("Empty Generic HackMap")(rdmGenericHackMap)
-  val rdmSpecializedGen = Gen.single("Empty Specilaized HackMap")(rdmSpecializedHackMap)
-  val rdmMiniboxedGen = Gen.single("Empty Miniboxed HackMap")(rdmMiniboxedHackMap)
-  val rdmJavaGen = Gen.single("Empty Java HashMap")(rdmJavaHashMap)
-
-  val size = 500000
-
-  // variable result in order to avoid in-lining
   var result = 0.0d
-  performance of "Generic HackMap" in {
-    measure method "put" in {
-      using(genericGen) config (testSettings: _*) in {
-        hm =>
-          val random = rand(0)
-          var i = 0
-          while (i < size) {
-            result += hm.put(random.nextInt, random.nextDouble)
-            i += 1
-          }
+  performance of "HashMaps" in {
+    performance of "Freezing HackMap" config (testSettings: _*) in {
+      measure method "put" in {
+        using(size) setUp {
+          size =>
+            System.gc()
+            hackMap = new HackMapGeneric[Int, Double]
+        } in {
+          size =>
+            val random = rand(0)
+            for (i <- 0 until size) {
+              hackMap.put(random.nextInt, random.nextDouble)
+            }
+            hackMap = null
+        }
+      }
+
+      measure method "get" in {
+        using(size) setUp {
+          size =>
+            System.gc()
+            hackMap = new HackMapGeneric[Int, Double]
+            val random = rand(1)
+            for (i <- 0 until size/2) {
+              hackMap.put(random.nextInt, random.nextDouble)
+            }
+        } in {
+          size =>
+            val random = rand(0)
+            for (i <- 0 until size) {
+              result += hackMap.get(random.nextInt)
+            }
+            hackMap = null
+        }
       }
     }
-
-    measure method "get" in {
-      using(rdmGenericGen) config (testSettings: _*) in {
-        hm =>
-          val random = rand(0)
-          var i = 0
-          while (i < size) {
-            result += hm.get(random.nextInt)
-            i += 1
-          }
+    performance of "Java HashMap" in {
+      measure method "put" in {
+        using(size) setUp {
+          size =>
+            System.gc()
+            hashMap = new stolen.HashMap[Int, Double]
+        } in {
+          size =>
+            val random = rand(0)
+            for (i <- 0 until size) {
+              hashMap.put(random.nextInt, random.nextDouble)
+            }
+            hashMap = null
+        }
       }
-    }
-  }
 
-  performance of "Specialized HackMap" in {
-    measure method "put" in {
-      using(specializedGen) config (testSettings: _*) in {
-        hm =>
-          val random = rand(0)
-          var i = 0
-          while (i < size) {
-            result += hm.put(random.nextInt, random.nextDouble)
-            i += 1
-          }
-      }
-    }
-
-    measure method "get" in {
-      using(rdmSpecializedGen) config (testSettings: _*) in {
-        hm =>
-          val random = rand(0)
-          var i = 0
-          while (i < size) {
-            result += hm.get(random.nextInt)
-            i += 1
-          }
-      }
-    }
-  }
-
-  performance of "Miniboxed HackMap" in {
-    measure method "put" in {
-      using(miniboxedGen) config (testSettings: _*) in {
-        hm =>
-          val random = rand(0)
-          var i = 0
-          while (i < size) {
-            result += hm.put(random.nextInt, random.nextDouble)
-            i += 1
-          }
-      }
-    }
-
-    measure method "get" in {
-      using(rdmMiniboxedGen) config (testSettings: _*) in {
-        hm =>
-          val random = rand(0)
-          var i = 0
-          while (i < size) {
-            result += hm.get(random.nextInt)
-            i += 1
-          }
-      }
-    }
-  }
-
-  performance of "Java HashMap" in {
-    measure method "put" in {
-      using(javaGen) config (testSettings: _*) in {
-        hm =>
-          val random = rand(0)
-          var i = 0
-          while (i < size) {
-            result += hm.put(random.nextInt, random.nextDouble)
-            i += 1
-          }
-      }
-    }
-
-    measure method "get" in {
-      using(rdmJavaGen) config (testSettings: _*) in {
-        hm =>
-          val random = rand(0)
-          var i = 0
-          while (i < size) {
-            result += hm.get(random.nextInt)
-            i += 1
-          }
+      measure method "get" in {
+        using(size) setUp {
+          size =>
+            System.gc()
+            hashMap = new stolen.HashMap[Int, Double]
+            val random = rand(1)
+            for (i <- 0 until size/2) {
+              hashMap.put(random.nextInt, random.nextDouble)
+            }
+        } in {
+          size =>
+            val random = rand(0)
+            for (i <- 0 until size) {
+              result += hashMap.get(random.nextInt)
+            }
+            hashMap = null
+        }
       }
     }
   }
--- components/scratchpad/test/miniboxing/test/scalatests/HackMapTest.scala
+++ components/scratchpad/test/miniboxing/test/scalatests/HackMapTest.scala
@@ -1,74 +1,74 @@
 package miniboxing.test.scalatests
 
 import org.scalatest.FunSuite
-import miniboxing.hackmap.HackMap
+import miniboxing.hackmap._
 
 class HackMapTest extends FunSuite {
 
   test("Generic HackMap Sanity check") {
     val random = new scala.util.Random(0)
-    val hm = new HackMap.Generic[Byte, Byte]
-    
+    val hm = new HackMapGeneric[Byte, Byte]
+
     for (i <- 0 until 1000) {
       hm.put(random.nextInt.toByte, random.nextInt.toByte)
     }
-    
+
     var result = 0.0d
     for (i <- 0 until 1000) {
       result += hm.get(random.nextInt.toByte)
     }
-    
+
     println(hm.size)
     println(result)
   }
-  
+
   test("Specialized HackMap Sanity check") {
     val random = new scala.util.Random(0)
-    val hm = new HackMap.Specialized[Byte, Byte]
-    
+    val hm = new HackMapSpecialized[Byte, Byte]
+
     for (i <- 0 until 1000) {
       hm.put(random.nextInt.toByte, random.nextInt.toByte)
     }
-    
+
     var result = 0.0d
     for (i <- 0 until 1000) {
       result += hm.get(random.nextInt.toByte)
     }
-    
+
     println(hm.size)
     println(result)
   }
-  
+
   test("Miniboxed HackMap Sanity check") {
     val random = new scala.util.Random(0)
-    val hm = new HackMap.Miniboxed[Byte, Byte]
-    
+    val hm = new HackMapMiniboxed[Byte, Byte]
+
     for (i <- 0 until 1000) {
       hm.put(random.nextInt.toByte, random.nextInt.toByte)
     }
-    
+
     var result = 0.0d
     for (i <- 0 until 1000) {
       result += hm.get(random.nextInt.toByte)
     }
-    
+
     println(hm.size)
     println(result)
   }
-  
+
   test("Java HashMap Sanity check") {
     val random = new scala.util.Random(0)
     val hm = new java.util.HashMap[Byte, Byte]
-    
+
     for (i <- 0 until 1000) {
       hm.put(random.nextInt.toByte, random.nextInt.toByte)
     }
-    
+
     var result = 0.0d
     for (i <- 0 until 1000) {
       result += hm.get(random.nextInt.toByte)
     }
-    
+
     println(hm.size())
     println(result)
   }
